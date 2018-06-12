fd057851775deb400cb557b8ecdbcd09bd234c69
Eduardo Padilla Cruz <eduardopadillacz@gmail.com>
2018-06-12 15:38:19 -0500

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$dominio="http://localhost/";
-//$dominio="http://adminave.pvessy.com/";
+//$dominio="http://localhost/";
+$dominio="http://adminave.pvessy.com/";
 $config['base_url'] = $dominio.'Avedesa/';
 
 /*
